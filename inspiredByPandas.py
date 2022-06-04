c1efac36ac178fbc4c5e7ff68d893b1dc4eeaf56
Francesco Luzzi <81475751+FrancescoLuzzi@users.noreply.github.com>
2022-06-04 16:14:16 +0200

    Refactor variables name

--- inspiredByPandas.py
+++ inspiredByPandas.py
@@ -8,51 +8,51 @@ counter = itertools.count()
 
 class Element:
     id: int
-    numero: int
-    messaggio: str
+    number: int
+    message: str
 
-    def __init__(self, numero: int, messaggio: str) -> None:
+    def __init__(self, number: int, message: str) -> None:
         self.id = next(counter)
-        self.numero = numero
-        self.messaggio = messaggio
+        self.number = number
+        self.message = message
 
 
 class SmartElement(Element):
     def __eq__(self, __o: Element | int) -> bool:
         if type(__o) == int:
-            return self.numero == __o
-        return self.id == __o.numero
+            return self.number == __o
+        return self.id == __o.number
 
     def __ne__(self, __o: Element | int) -> bool:
         return not self.__eq__(__o)
 
     def __gt__(self, __o: Element | int) -> bool:
         if type(__o) == int:
-            return self.numero > __o
-        return self.numero > __o.numero
+            return self.number > __o
+        return self.number > __o.number
 
     def __lt__(self, __o: Element | int) -> bool:
         if type(__o) == int:
-            return self.numero < __o
-        return self.numero < __o.numero
+            return self.number < __o
+        return self.number < __o.number
 
     def __ge__(self, __o: Element | int) -> bool:
         if type(__o) == int:
-            return self.numero >= __o
-        return self.numero >= __o.numero
+            return self.number >= __o
+        return self.number >= __o.number
 
     def __le__(self, __o: Element | int) -> bool:
         if type(__o) == int:
-            return self.numero <= __o
-        return self.numero <= __o.numero
+            return self.number <= __o
+        return self.number <= __o.number
 
     def __mod__(self, __o: Element | int) -> int:
         if type(__o) == int:
-            return self.numero % __o
-        return self.numero % __o.numero
+            return self.number % __o
+        return self.number % __o.number
 
     def __repr__(self) -> str:
-        return f"{self.messaggio} -> {self.numero}"
+        return f"{self.message} -> {self.number}"
 
 
 class IdSeries(tuple):
@@ -67,17 +67,17 @@ class IdSeries(tuple):
 
 
 class ElementList:
-    prove: list[SmartElement] = []
+    elements: list[SmartElement] = []
 
-    def __init__(self, lista: list[SmartElement]) -> None:
-        self.prove = lista
+    def __init__(self, list: list[SmartElement]) -> None:
+        self.elements = list
 
 
 class SmartElementList(ElementList):
     def __contains__(self, key):
         key_type = type(key)
         if key_type == SmartElement:
-            for el in self.prove:
+            for el in self.elements:
                 if el.id == key.id:
                     return True
             return False
@@ -87,74 +87,74 @@ class SmartElementList(ElementList):
     def __getitem__(self, key):
         key_type = type(key)
         if key_type == int:
-            return self.__class__([self.prove[key]])
+            return self.__class__([self.elements[key]])
         elif key_type == slice:
-            return self.__class__(self.prove[key])
+            return self.__class__(self.elements[key])
         elif key_type == IdSeries:
-            return self.__class__([el for el in self.prove if el in key])
+            return self.__class__([el for el in self.elements if el in key])
         elif issubclass(key.__class__, ElementList):
-            return self.__class__([el for el in self.prove if el in key])
+            return self.__class__([el for el in self.elements if el in key])
         else:
             print(f"No implementation for type {key_type}")
 
     def __eq__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el == __o)
+        return IdSeries(el.id for el in self.elements if el == __o)
 
     def __ne__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el != __o)
+        return IdSeries(el.id for el in self.elements if el != __o)
 
     def __gt__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el > __o)
+        return IdSeries(el.id for el in self.elements if el > __o)
 
     def __lt__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el < __o)
+        return IdSeries(el.id for el in self.elements if el < __o)
 
     def __ge__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el >= __o)
+        return IdSeries(el.id for el in self.elements if el >= __o)
 
     def __le__(self, __o: int) -> "IdSeries":
-        return IdSeries(el.id for el in self.prove if el <= __o)
+        return IdSeries(el.id for el in self.elements if el <= __o)
 
     def eq(self, __o: int | Element) -> "SmartElementList":
-        return self.__class__([el for el in self.prove if el == __o])
+        return self.__class__([el for el in self.elements if el == __o])
 
     def ne(self, __o: int | Element) -> "SmartElementList":
-        return self.__class__([el for el in self.prove if el != __o])
+        return self.__class__([el for el in self.elements if el != __o])
 
     def gt(self, __o: int | Element) -> "SmartElementList":
-        return self.__class__([el for el in self.prove if el > __o])
+        return self.__class__([el for el in self.elements if el > __o])
 
     def lt(self, __o: int | Element) -> "SmartElementList":
-        return self.__class__([el for el in self.prove if el < __o])
+        return self.__class__([el for el in self.elements if el < __o])
 
     def ge(self, __o: int | Element) -> "SmartElementList":
-        return self.__class__([el for el in self.prove if el >= __o])
+        return self.__class__([el for el in self.elements if el >= __o])
 
     def le(self, __o: int | Element):
-        return self.__class__([el for el in self.prove if el <= __o])
+        return self.__class__([el for el in self.elements if el <= __o])
 
     def copy(self):
-        return self.__class__([copy(el) for el in self.prove])
+        return self.__class__([copy(el) for el in self.elements])
 
     def __mod__(self, __o: int):
         out = []
-        for el in self.copy().prove:
-            el.numero = el % __o
+        for el in self.copy().elements:
+            el.number = el % __o
             out.append(el)
         return self.__class__(out)
 
     def __repr__(self) -> str:
-        return f"{self.prove}"
+        return f"{self.elements}"
 
     def scramble(self):
         random.seed(time_ns())
-        random.shuffle(self.prove)
+        random.shuffle(self.elements)
 
     def sort(self, reverse: bool = False):
-        self.prove.sort(reverse=reverse)
+        self.elements.sort(reverse=reverse)
 
 
-lista = [
+element_list = [
     SmartElement(1, "Hey"),
     SmartElement(2, "Hey"),
     SmartElement(3, "You"),
@@ -163,7 +163,7 @@ lista = [
     SmartElement(6, "World"),
 ]
 
-prove = SmartElementList(lista)
+elements = SmartElementList(element_list)
 
-print(prove[prove % 2 == 1])
-print(prove[prove == 1])
+print(elements[elements % 2 == 1])
+print(elements[elements == 1])
